399f5c5bd57ea317def57f18a6f7febd9a4d3be8
Malik-Rizwan125 <99021298+Malik-Rizwan125@users.noreply.github.com>
2022-08-25 12:19:23 +0500

    2questions are done and3rd question start

---
+++ Q3-negative-positive-or-zero.js
@@ -0,0 +1,5 @@
+// 3. Write a ts program to check whether a number is negative, positive or zero.
+var num = 5;
+if (num % 2 == 0) {
+    console.log("number is positive");
+}
---
+++ Q3-negative-positive-or-zero.ts
@@ -0,0 +1,6 @@
+// 3. Write a ts program to check whether a number is negative, positive or zero.
+
+var num:number = 5;
+if(num % 2 == 0){
+    console.log("number is positive")
+}
